7dff2aad618d6f761f4fc9c7eee41284c8cebf69
test-apps-rkulikov <kulikov33.roman@gmail.com>
2019-04-22 16:04:10 +0300

    Add app to package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='statistic',
+      version='1.0',
+      description='Mail server statistics',
+      classifiers=[
+        'Programming Language :: Python :: 3.6',
+      ],
+      url='https://github.com/PochtyDendi/Mail_server_statistics',
+      author='Kulikov Roman',
+      author_email='Kulikov-roman@myrambler.ru',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
